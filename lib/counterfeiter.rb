05172366f32194a619d4c8b6f770633c42b668e4
Julien Pellet <pelletj@gmail.com>
2014-03-23 22:45:17 +0100

    store path and file in redis

--- lib/counterfeiter.rb
+++ lib/counterfeiter.rb
@@ -1,7 +1,7 @@
 require 'redis'
 
 class Counterfeiter
-  @REDIS_SET='Counterfeiter'
+  @redis_key='Counterfeiter'
 
   def self.upload_new_files target_dir, last_update = Time.at(0)
     Dir.glob(File.join(target_dir,'**/*')).each do |f|
@@ -16,19 +16,13 @@ class Counterfeiter
   end
 
   def self.upload_file_to_redis file, time
-    @redis.zadd @REDIS_SET, time, File.read(file)
+    @redis.zadd @redis_key, time, {path:file, body: File.read(file)}
   end
 
   def self.download_new_files last_update = Time.at(0)
-    files = @redis.zrangebyscore(@REDIS_SET, last_update.to_i, Time.now.utc.to_i)
-    puts files
+    files = @redis.zrangebyscore(@redis_key, last_update.to_i, Time.now.utc.to_i)
+    files.each do |f|
+      puts eval(f)[:path]
+    end
   end
-
-  # def __get_new_files target_dir, last_update=Time.at(0)
-  #   files = []
-  #   Dir.glob(File.join(target_dir,'**/*')) do |f|
-  #     files << f if File.mtime(f) > last_update
-  #   end
-  #   return files
-  # end
 end
